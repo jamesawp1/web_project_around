f4603539bd8b13d4cd5cc6edbec3b5a05d615f6c
jamesawp1 <jamesawp@protonmail.com>
2024-04-12 16:17:29 -0300

    fix some bugs in the js script

--- scripts/index.js
+++ scripts/index.js
@@ -4,22 +4,48 @@ const closePopupButton = document.querySelector(".popup__close-button");
 
 editButton.addEventListener("click", () => {
   popup.classList.add("popup_opened");
-  validationForm(
-    "#input-name",
-    "#input-role",
-    "#input-name-error",
-    "#input-role-error",
-    "#save-button"
-  );
 
   closePopupButton.addEventListener("click", () => {
     popup.classList.remove("popup_opened");
-    closePopupButton.removeEventListener();
   });
 });
 
-popup.addEventListener("click", (event) => {
-  if (event.target.classList.contains("popup")) {
-    popup.classList.remove("popup_opened");
-  }
+// Vamos encontrar o formulário no DOM
+
+const formElement = document.querySelector(".popup__form");
+
+// Em seguida vem o handler do submit
+// ainda não vai enviar para lugar nenhum
+
+// Observe que o nome da função começa com um verbo
+// e descreve exatamente o que a função faz
+function handleProfileFormSubmit(evt) {
+  // Esta linha impede o navegador
+  // de enviar o formulário da forma padrão.
+  evt.preventDefault();
+  // Fazendo isso, podemos definir nossa própria forma de enviar o formulário.
+  // Explicaremos em mais detalhes posteriormente.
+
+  // Vamos encontrar os campos de formulário do DOM
+  const nameInput = document.querySelector("#input-name");
+  const roleInput = document.querySelector("#input-role");
+
+  // Pegue os valores de cada campo do valor da propriedade correspondente
+  const name = nameInput.value;
+  const role = roleInput.value;
+  // Selecione os elementos aos quais os valores dos campos serão inseridos
+  const editedName = document.querySelector(".profile__title");
+  const editedRole = document.querySelector(".profile__subtitle");
+  // Insira novos valores usando a
+  // propriedade textContent
+  editedName.textContent = name;
+  editedRole.textContent = role;
+}
+
+// Conecte o handler ao formulário:
+// ele vai observar o evento de submit
+
+//formElement.addEventListener("submit", handleProfileFormSubmit);
+formElement.addEventListener("submit", (handleProfileFormSubmit) => {
+  popup.classList.remove("popup_opened");
 });
