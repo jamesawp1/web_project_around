2523505823fe3c2e92e0857fe9b0500de0e3b1ff
jamesawp1 <jamesawp@protonmail.com>
2024-05-02 23:40:33 -0300

    Refactor the js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,23 @@
 const editButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const closePopupButton = document.querySelector(".popup__close-button");
-
-editButton.addEventListener("click", () => {
-  popup.classList.add("popup_opened");
+editButton.addEventListener("click", function () {
+  popup.classList.toggle("popup_opened");
 });
-
-closePopupButton.addEventListener("click", () => {
-  popup.classList.remove("popup_opened");
+closePopupButton.addEventListener("click", function () {
+  popup.classList.toggle("popup_opened");
   closePopupButton.removeEventListener();
 });
 
 const formElement = document.querySelector(".popup__form");
-
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  const nameInput = document.querySelector("#input-name");
-  const roleInput = document.querySelector("#input-role");
+  let nameInput = document.querySelector("#input-name");
+  let jobInput = document.querySelector("#input-job");
   const profileName = document.querySelector(".profile__title");
-  const profileRole = document.querySelector(".profile__subtitle");
+  const profileJob = document.querySelector(".profile__subtitle");
   profileName.textContent = nameInput.value;
-  profileRole.textContent = roleInput.value;
-  popup.classList.remove("popup_opened");
+  profileJob.textContent = jobInput.value;
+  popup.classList.toggle("popup_opened");
 }
-
 formElement.addEventListener("submit", handleProfileFormSubmit);
