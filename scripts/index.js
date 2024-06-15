77f7a0dbe78c231e13b528c42be2a395dcd957d6
jamesawp1 <jamesawp@protonmail.com>
2024-06-14 21:16:33 -0300

    Create the deleteButton functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 const editButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const closePopupButton = document.querySelector(".popup__close-button");
+
 editButton.addEventListener("click", function () {
   popup.classList.toggle("popup_opened");
 });
 closePopupButton.addEventListener("click", function () {
   popup.classList.toggle("popup_opened");
-  closePopupButton.removeEventListener();
 });
 
 const formElement = document.querySelector(".popup__form");
@@ -21,3 +21,61 @@ function handleProfileFormSubmit(evt) {
   popup.classList.toggle("popup_opened");
 }
 formElement.addEventListener("submit", handleProfileFormSubmit);
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
+const template = document.querySelector("#template").content;
+const cardElement = template.querySelector(".gallery__card");
+
+function renderCard(card) {
+  const cardClone = cardElement.cloneNode(true);
+
+  cardClone.querySelector(".gallery__card-name").textContent = card.name;
+
+  cardClone
+    .querySelector(".gallery__card-image")
+    .setAttribute("src", card.link);
+
+  cardClone
+    .querySelector(".gallery__card-image")
+    .setAttribute("alt", card.name);
+  //Excluí o card
+  const deleteButton = cardClone.querySelector(".gallery__delete-icon");
+  deleteButton.addEventListener("click", () => {
+    deleteButton.closest(".gallery__card").remove();
+  });
+
+  return cardClone;
+}
+
+const gallery = document.querySelector(".gallery");
+
+initialCards.forEach((card, index) => {
+  const cardItem = renderCard(card);
+  gallery.append(cardItem);
+});
