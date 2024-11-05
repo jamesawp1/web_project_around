5571fb99abc7bc5f456658d33faa82901729d88d
jamesawp1 <jamesawp@protonmail.com>
2024-11-05 14:47:48 -0300

    Create the private method _showInputError

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -6,4 +6,13 @@ class FormValidator {
     this._errorVisible = selectors.errorVisible;
     this._errorMessage = selectors.errorMessage;
   }
+
+  _showInputError = () => {
+    this._errorElement = this._formElement.querySelector(
+      `.${this._inputElement.id}-error`
+    );
+    this._inputElement.classList.add(`${this._inputError}`);
+    this._errorElement.textContent = this._errorMessage;
+    this._errorElement.classList.add(`${this._errorVisible}`);
+  };
 }
