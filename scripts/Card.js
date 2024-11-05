528ab7a647e8238971ccc34a57f6133a832033cc
jamesawp1 <jamesawp@protonmail.com>
2024-11-05 10:37:31 -0300

    Structure the method that iterates through the initialCards array

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
 class Card {
   constructor(text, image, selector) {
     this._text = text;
@@ -7,7 +34,7 @@ class Card {
 
   _getElement() {
     const cardElement = document
-      .querySelector("#template")
+      .querySelector(this._selector)
       .content.cloneNode(true);
 
     return cardElement;
@@ -27,3 +54,11 @@ class Card {
     return this._element;
   }
 }
+
+initialCards.forEach((item) => {
+  const card = new Card(item.name, item.link, "#template");
+
+  const cardElement = card.generateCard();
+
+  document.querySelector(".gallery").append(cardElement);
+});
