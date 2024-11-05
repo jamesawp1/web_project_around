8befeb72c45a5183316335d70e1b25b9257e849f
jamesawp1 <jamesawp@protonmail.com>
2024-11-05 14:56:31 -0300

    Create the private method _hasInvalidInput

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -7,29 +7,35 @@ class FormValidator {
     this._errorMessage = selectors.errorMessage;
   }
 
-  _showInputError = () => {
+  _showInputError() {
     this._errorElement = this._formElement.querySelector(
       `.${this._inputElement.id}-error`
     );
     this._inputElement.classList.add(`${this._inputError}`);
     this._errorElement.textContent = this._errorMessage;
     this._errorElement.classList.add(`${this._errorVisible}`);
-  };
+  }
 
-  _hideInputError = () => {
+  _hideInputError() {
     this._errorElement = this._formElement.querySelector(
       `.${this._inputElement.id}-error`
     );
     this._inputElement.classList.remove(`${this._inputError}`);
     this._errorElement.textContent = "";
     this._errorElement.classList.remove(`${this._errorVisible}`);
-  };
+  }
 
-  _checkInputValidity = () => {
+  _checkInputValidity() {
     if (!this._inputElement.validity.valid) {
       showInputError();
     } else {
       hideInputError();
     }
-  };
+  }
+
+  _hasInvalidInput(inputList) {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
 }
