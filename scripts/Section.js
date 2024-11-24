fa52b1b277ddee3b2ba659d9fce38bd79bda6138
jamesawp1 <jamesawp@protonmail.com>
2024-11-24 08:40:43 -0300

    Create renderItems public method

--- scripts/Section.js
+++ scripts/Section.js
@@ -3,4 +3,10 @@ class Section {
     this._items = items;
     this._renderer = renderer;
   }
+
+  renderItems() {
+    this._items.forEach((item) => {
+      this._renderer(item);
+    });
+  }
 }
