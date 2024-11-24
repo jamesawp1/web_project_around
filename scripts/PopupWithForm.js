48c581252201c09dc73d3bb69d458033f81a0592
jamesawp1 <jamesawp@protonmail.com>
2024-11-24 10:25:54 -0300

    Add the getInputValues private method

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -5,4 +5,16 @@ class PopupWithForm extends Popup {
     super(popupSelector);
     this._submit = submitFunction;
   }
+
+  _getInputValues() {
+    this._inputList = this._selector.querySelectorAll(".popup__input");
+
+    this._formValues = {};
+
+    this._inputList.forEach((input) => {
+      this._formValues[input.name] = input.value;
+    });
+
+    return this._formValues;
+  }
 }
