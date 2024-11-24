16155512cc69b85912017e00d8200d817dfb7542
jamesawp1 <jamesawp@protonmail.com>
2024-11-24 08:59:58 -0300

    Add the open and close public methods

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -2,4 +2,12 @@ class Popup {
   constructor(popupSelector) {
     this._selector = popupSelector;
   }
+
+  open() {
+    this._selector.classList.add("popup_opened");
+  }
+
+  close() {
+    this._selector.classList.remove("popup_opened");
+  }
 }
