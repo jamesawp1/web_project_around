ecb10131a757044f2440b26fcda211a9358ec09b
jamesawp1 <jamesawp@protonmail.com>
2024-11-24 08:42:40 -0300

    Create addItem public method

--- scripts/Section.js
+++ scripts/Section.js
@@ -2,11 +2,16 @@ class Section {
   constructor({ items, renderer }, targetSelector) {
     this._items = items;
     this._renderer = renderer;
+    this._selector = targetSelector;
   }
 
   renderItems() {
     this._items.forEach((item) => {
       this._renderer(item);
     });
+  }
+
+  addItem(element) {
+    this._selector.append(element);
   }
 }
