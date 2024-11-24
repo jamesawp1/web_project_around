1189e67a46688d63ff3b3a01815d93100b456057
jamesawp1 <jamesawp@protonmail.com>
2024-11-24 10:08:20 -0300

    Finished the PopupWithImage class

--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -4,4 +4,13 @@ class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
   }
+
+  open(item) {
+    this._selector.querySelector(".popup-view-image__image").src = item.link;
+    this._selector.querySelector(".popup-view-image__image").alt = item.name;
+    this._selector.querySelector(".popup-view-image__title").textContent =
+      item.name;
+
+    super.open();
+  }
 }
