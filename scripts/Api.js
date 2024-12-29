2adb45296935406ae8c6e12179acf85242b397bb
jamesawp1 <jamesawp@protonmail.com>
2024-12-29 09:42:41 -0300

    Create the api class and its getUserIndo method

---
+++ scripts/Api.js
@@ -0,0 +1,16 @@
+class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    });
+  }
+}
